8012f4ec188ceb6be32c3c30da3059f34f168e6d
Nicholas White <n6white@uwaterloo.ca>
2017-10-25 21:03:49 -0400

    Parse Spotify links to gpm

--- test.py
+++ test.py
@@ -5,4 +5,8 @@ from link_converter.LinkConverter import LinkConverter
 
 if __name__ == "__main__":
     linkConverter = LinkConverter()
-    linkConverter.convert_link('https://play.google.com/music/m/Todhdrnwy3w3clgsqttgsbrklqi?t=This_Is_the_Beginning_-_Shakey_Graves')
+    links1 = linkConverter.convert_link('https://play.google.com/music/m/Todhdrnwy3w3clgsqttgsbrklqi?t=This_Is_the_Beginning_-_Shakey_Graves')
+    links2 = linkConverter.convert_link('https://open.spotify.com/track/0GswbpVTZBVwvE4gZLaX7R')
+
+    print(links1)
+    print(links2)
