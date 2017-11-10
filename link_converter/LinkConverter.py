10f536b5fcd5125372c956fb987e274e3ec57293
Nick White <nick41496@gmail.com>
2017-11-10 03:03:54 -0500

    Add album conversion functionality

--- link_converter/LinkConverter.py
+++ link_converter/LinkConverter.py
@@ -15,6 +15,11 @@ class LinkConverter:
 
     gpm_format = "https://play.google.com/music/m/{0}"
 
+    gpm_album_prefix = 'B'
+    gpm_track_prefix = 'T'
+    spotify_album_prefix = 'album'
+    spotify_track_prefix = 'track'
+
     def __init__(self):
         self.gpm_api = self.get_gpm_api()
         self.spotify_api = self.get_spotify_api()
@@ -46,34 +51,65 @@ class LinkConverter:
             raise ValueError("Received a link I can't handle: {0}".format(link))
 
     def parse_gpm_link(self, url):
-        gpm_link = parse.urlunparse(url) 
-        song_info = url.query.replace("t=", '').replace('_', ' ').split(" - ")
-        spotify_link = self.get_spotify_link(song_info)
+        # item could be either a track or an album
+        item_id = url.path.split('/')[-1]
+        gpm_link = '{0}://{1}{2}'.format(url.scheme, url.netloc, url.path)
+        prefix = item_id[:1]
+        if prefix == self.gpm_album_prefix:
+            album = self.gpm_api.get_album_info(item_id)
+            album_info = (album['name'], album['artist'])
+            spotify_link = self.get_spotify_album(album_info)
+        elif prefix == self.gpm_track_prefix:
+            track = self.gpm_api.get_track_info(item_id)
+            track_info = (track['title'], track['artist'])
+            spotify_link = self.get_spotify_track(track_info)
         return (gpm_link, spotify_link)
 
     def parse_spotify_link(self, url):
+        # item could be either a track or an album
+        item_id = url.path.split('/')[-1]
         spotify_link = parse.urlunparse(url)
-        track_id = url.path.split('/')[2]
-        track = self.spotify_api.track(track_id)
-        song_info = (track['artists'][0]['name'], track['name'])
-        gpm_link = self.get_gpm_link(song_info)
+        prefix = url.path.split('/')[1] 
+        if prefix == self.spotify_album_prefix:
+            album = self.spotify_api.album(item_id)
+            album_info = (album['name'], album['artists'][0]['name'])
+            gpm_link = self.get_gpm_album(album_info)
+        elif prefix == self.spotify_track_prefix:
+            track = self.spotify_api.track(item_id)
+            track_info = (track['name'], track['artists'][0]['name'])
+            gpm_link = self.get_gpm_track(track_info)
         return (gpm_link, spotify_link)
 
-    def get_gpm_link(self, song_info):
-        query = "song:{1} {0}".format(song_info[0], song_info[1])
+    def get_gpm_album(self, album_info):
+        query = "{0} {1}".format(album_info[0], album_info[1])
+        results = self.gpm_api.search(query, max_results = 1)
+        if (len(results['album_hits']) > 0):
+            album_id = results['album_hits'][0]['album']['albumId']
+            return self.gpm_format.format(album_id)
+        else:
+            print("Could not find an album on Google Play using the following info:\n{0}".format(album_info))
+            return None
+
+    def get_gpm_track(self, track_info):
+        query = "song:{0} {1}".format(track_info[0], track_info[1])
         results = self.gpm_api.search(query, max_results = 1)
         if (len(results['song_hits']) > 0):
             track_id = "T{0}".format(results['song_hits'][0]['track']['nid'])
             return self.gpm_format.format(track_id)
         else:
-            print("Could not find a track on Google Play using the following info:\n{0}".format(song_info))
+            print("Could not find a track on Google Play using the following info:\n{0}".format(track_info))
             return None
 
-    def get_spotify_link(self, song_info):
-        query = "track:{0} artist:{1}".format(song_info[0], song_info[1])
+    def get_spotify_album(self, album_info):
+        query = "album:{0} artist:{1}".format(album_info[0], track_info[1])
+        results = self.spotify_api.search(query, limit = 10, type = "album")
+        print(results)
+
+    def get_spotify_track(self, track_info):
+        query = "track:{0} artist:{1}".format(track_info[0], track_info[1])
         results = self.spotify_api.search(query, limit = 10, type = "track")
         if (results['tracks']['total'] > 0):
             return results['tracks']['items'][0]['external_urls']['spotify']
         else:
-            print("Could not find a track on Spotify using the following info:\n{0}".format(song_info))
+            print("Could not find a track on Spotify using the following info:\n{0}".format(track_info))
             return None
--- test.py
+++ test.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     links1 = linkConverter.convert_link('https://play.google.com/music/m/Todhdrnwy3w3clgsqttgsbrklqi?t=This_Is_the_Beginning_-_Shakey_Graves')
     links2 = linkConverter.convert_link('https://open.spotify.com/track/0GswbpVTZBVwvE4gZLaX7R')
     links3 = linkConverter.convert_link('https://open.spotify.com/track/0KDn1UsD2ym34dCT4P9ebj')
-    # links4 = linkConverter.convert_link('https://open.spotify.com/album/439SsuO2Esw1G67Xuw1HPS')
+    links4 = linkConverter.convert_link('https://open.spotify.com/album/439SsuO2Esw1G67Xuw1HPS')
     links5 = linkConverter.convert_link('https://open.spotify.com/track/2Sm1PY9pcoQzFDL1NJsJj8')
 
     print(links1)
     print(links2)
     print(links3)
-    # print(links4)
+    print(links4)
     print(links5)
