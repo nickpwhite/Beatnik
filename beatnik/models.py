f8c968b3446870442d0d2b3a4afe769e0b6d415d
Nick White <nick41496@gmail.com>
2017-11-14 01:11:07 -0500

    Add title, artist, artwork to the models

--- beatnik/models.py
+++ beatnik/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Song(models.Model):
+    name = models.CharField("Track Name", max_length=200)
+    artist = models.CharField("Artist Name", max_length=200)
     gpm_id = models.CharField("Google Play Music ID", max_length=200)
     spotify_id = models.CharField("Spotify ID", max_length=200)
     apple_id = models.CharField("Apple Music ID", max_length=200)
@@ -8,8 +10,11 @@ class Song(models.Model):
     match_rating = models.IntegerField("Rating of the match", default=0)
     insertion_date = models.DateTimeField("Date of initial insertion")
     update_date = models.DateTimeField("Date of last update")
+    artwork = models.URLField()
 
 class Album(models.Model):
+    name = models.CharField("Album Name", max_length=200)
+    artist = models.CharField("Artist Name", max_length=200)
     gpm_id = models.CharField("Google Play Music ID", max_length=200)
     spotify_id = models.CharField("Spotify ID", max_length=200)
     apple_id = models.CharField("Apple Music ID", max_length=200)
@@ -17,3 +22,4 @@ class Album(models.Model):
     match_rating = models.IntegerField("Rating of the match", default=0)
     insertion_date = models.DateTimeField("Date of initial insertion")
     update_date = models.DateTimeField("Date of last update")
+    artwork = models.URLField()
