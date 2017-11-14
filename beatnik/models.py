b2ee00285b86bc47db4fb0e740807aa03f1c2d86
Nick White <nick41496@gmail.com>
2017-11-13 22:02:12 -0500

    Add inserted and updated dates to models

--- beatnik/models.py
+++ beatnik/models.py
@@ -6,6 +6,8 @@ class Song(models.Model):
     apple_id = models.CharField("Apple Music ID", max_length=200)
     soundcloud_id = models.CharField("Soundcloud ID", max_length=200)
     match_rating = models.IntegerField("Rating of the match", default=0)
+    insertion_date = models.DateTimeField("Date of initial insertion")
+    update_date = models.DateTimeField("Date of last update")
 
 class Album(models.Model):
     gpm_id = models.CharField("Google Play Music ID", max_length=200)
@@ -13,3 +15,5 @@ class Album(models.Model):
     apple_id = models.CharField("Apple Music ID", max_length=200)
     soundcloud_id = models.CharField("Soundcloud ID", max_length=200)
     match_rating = models.IntegerField("Rating of the match", default=0)
+    insertion_date = models.DateTimeField("Date of initial insertion")
+    update_date = models.DateTimeField("Date of last update")
