5e596c6e5e1dcbd4ffab564bf313a12387f921ef
Nick White <nick41496@gmail.com>
2017-11-13 21:52:22 -0500

    Add Album and Song models and migrations

---
+++ beatnik/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class BeatnikConfig(AppConfig):
+    name = 'beatnik'
--- beatnik/models.py
+++ beatnik/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Song(models.Model):
-    gpm_id = models.CharField(max_length=200)
-    spotify_id = models.CharField(max_length=200)
-    apple_id = models.CharField(max_length=200)
-    soundcloud_id = models.CharField(max_length=200)
+    gpm_id = models.CharField("Google Play Music ID", max_length=200)
+    spotify_id = models.CharField("Spotify ID", max_length=200)
+    apple_id = models.CharField("Apple Music ID", max_length=200)
+    soundcloud_id = models.CharField("Soundcloud ID", max_length=200)
+    match_rating = models.IntegerField("Rating of the match", default=0)
 
 class Album(models.Model):
-    gpm_id = models.CharField(max_length=200)
-    spotify_id = models.CharField(max_length=200)
-    apple_id = models.CharField(max_length=200)
-    soundcloud_id = models.CharField(max_length=200)
+    gpm_id = models.CharField("Google Play Music ID", max_length=200)
+    spotify_id = models.CharField("Spotify ID", max_length=200)
+    apple_id = models.CharField("Apple Music ID", max_length=200)
+    soundcloud_id = models.CharField("Soundcloud ID", max_length=200)
+    match_rating = models.IntegerField("Rating of the match", default=0)
