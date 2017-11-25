73b8d199aec58ab92c1e55b18d3de84e4ebb9754
Nick White <nick41496@gmail.com>
2017-11-25 01:32:52 -0500

    Create a content script for messenger.com

--- web_extension/default.js
+++ web_extension/default.js
@@ -1,23 +1,15 @@
-const beatnik_regex = /^http:\/\/127\.0\.0\.1:8000\/.*$/
+const regex = /https?:\/\/(?:play\.google\.com\/music\/m\/[A-z0-9]+(?:\?t=.*)?|open\.spotify\.com\/(?:track|album)\/[A-z0-9]+)/;
 
-const url = document.URL;
+let anchors = document.getElementsByTagName('a');
 
-if (beatnik_regex.exec(url)) {
-    console.log("Hello Beatnik user!");
-} else {
-    const regex = /https?:\/\/(?:play\.google\.com\/music\/m\/[A-z0-9]+(?:\?t=.*)?|open\.spotify\.com\/(?:track|album)\/[A-z0-9]+)/;
+for (let i = 0; i < anchors.length; i++) {
+    let href = anchors[i].href;
 
-    let anchors = document.getElementsByTagName('a');
-
-    for (let i = 0; i < anchors.length; i++) {
-        let href = anchors[i].href;
-
-        if (regex.exec(href)) {
-            new_href = `http://127.0.0.1:8000/music/?q=${href}`;
-            console.log(new_href);
-            anchors[i].href = new_href;
-        } else {
-            console.log(href);
-        }
+    if (regex.exec(href)) {
+        new_href = `http://127.0.0.1:8000/music/?q=${href}`;
+        console.log(new_href);
+        anchors[i].href = new_href;
+    } else {
+        console.log(href);
     }
 }
---
+++ web_extension/messenger.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", replaceLinks, false);
+
+function replaceLinks(event) {
+    const link_regex = /https:\/\/l\.messenger\.com\/l\.php\?u=([A-z\.\%0-9]+)&h=.+$/;
+    const regex = /https?:\/\/(?:play\.google\.com\/music\/m\/[A-z0-9]+(?:\?t=.*)?|open\.spotify\.com\/(?:track|album)\/[A-z0-9]+)/;
+
+    let anchors = document.getElementsByTagName('a');
+
+    for (let i = 0; i < anchors.length; i++) {
+        let result = link_regex.exec(anchors[i].href);
+
+        if (result) {
+            let href = decodeURIComponent(result[1]);
+
+            if (regex.exec(href)) {
+                new_href = `http://127.0.0.1:8000/music/?q=${href}`;
+                console.log(new_href);
+                anchors[i].href = new_href;
+            } else {
+                console.log(href);
+            }
+        } else {
+            console.log(anchors[i]);
+        }
+    }
+}
