0e0e8684b461b8d8fadcee95c899dfa760296818
Nick White <nick41496@gmail.com>
2018-01-11 10:59:16 -0500

    Get favicon working

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((<BrowserRouter><App /></BrowserRouter>), document.getElementById('root'));
-registerServiceWorker();
