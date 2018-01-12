04241926ee4ed720531263753d417bff336b9611
Nick White <nick41496@gmail.com>
2018-01-12 18:46:32 -0500

    Fix some mobile styling

--- reactjs/src/Header.js
+++ reactjs/src/Header.js
@@ -28,14 +28,14 @@ class Header extends Component {
     return (
       <header className="header">
         <div className="flex-row">
-          <div className="col-25">
+          <div className="col-33">
             <Link to="/index" className="title"><h1>Beatnik</h1></Link>
           </div>
-          <div className="col-25 subtitle">
+          <div className="col-33 subtitle">
             <Link to="/convert" className="title"><h2>Link Converter</h2></Link>
           </div>
           { this.state.ext_url && 
-            <div className="col-25 subtitle">
+            <div className="col-33 subtitle">
               <a href={ this.state.ext_url } target="_blank" className="title"><h2>Get the extension</h2></a>
             </div>
           }
