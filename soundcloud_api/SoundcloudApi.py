ac893354b05692a6956a7614d42c409b30ddc3b6
Nick White <nick41496@gmail.com>
2018-03-05 13:50:13 -0500

    Fix some Soundcloud stuff

--- soundcloud_api/SoundcloudApi.py
+++ soundcloud_api/SoundcloudApi.py
@@ -10,13 +10,16 @@ class SoundcloudApi:
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.query_url = 'https://soundcloud.com/search?q={0}%20{1}'
+        self.query_url = 'https://soundcloud.com/search?q={0}+{1}'
 
     def get(self, url):
         d = pq(url=url)
         music_type = "album" if url.split('/')[4] == "sets" else "track"
         info = d("h1 > a")
+        title = info.eq(0).html()
+        artist = info.eq(1).html()
         artwork = d("img").eq(1).attr("src")
+        self.logger.info('title: {0} artist: {1}'.format(title, artist))
         return {
             'type': music_type,
             'title': info.eq(0).html(),
@@ -25,23 +28,21 @@ class SoundcloudApi:
         }
 
     def search(self, title, artist):
-        query = self.query_url.format(title, artist).replace(' ', '%20')
+        clean_title = title.replace(' & ', '+')
+        clean_artist = artist.replace(' & ', '+')
+        query = self.query_url.format(clean_title, clean_artist).replace(' ', '+')
         self.logger.info('query: {0}'.format(query))
         d = pq(url=query)
         
-        expected_artist = artist.translate(self.TRANS_TABLE).lower()
-        expected_title = title.translate(self.TRANS_TABLE).lower()
+        expected_artist = clean_artist.lower()
+        expected_title = clean_title.lower()
         results = d("ul:last > li > h2 > a")
-
-        self.logger.info('exp_artist: {0}, exp_title: {1}'.format(expected_artist, expected_title))
 
         for item in results.items():
             text = item.text().lower()
-            acutal_artist, actual_title, *other = text.split(' - ')
-            actual_artist = acutal_artist.translate(self.TRANS_TABLE)
-            actual_title = actual_title.translate(self.TRANS_TABLE)
-            self.logger.info('artist: {0}, title: {1}'.format(actual_artist, actual_title))
-            if expected_artist in actual_artist and expected_title in actual_title:
+            self.logger.info('text: {0}'.format(text))
+            self.logger.info('title: {0}'.format(title))
+            if title.lower() in text:
                 href = 'https://soundcloud.com{0}'.format(item.attr.href)
                 return href 
 
