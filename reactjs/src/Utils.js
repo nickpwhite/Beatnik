c6e05389be2e0001dce2b5e6e64b4ec1d8c9019c
Nick White <nick41496@gmail.com>
2018-09-04 23:10:27 -0700

    Don't break the site

--- reactjs/src/Utils.js
+++ reactjs/src/Utils.js
@@ -2,17 +2,19 @@ class Utils {
   static getQueryParams(url) {
     if(!url) url = window.location.href;
 
-    const matches = url.match(/[^?]*\?(?:([^=]+)=([^&]+))+/).slice(1);
+    const matches = url.match(/[^?]*\?(?:([^=]+)=([^&]+))+/)
+      
+    const groups = matches ? matches.slice(1) : {};
 
-    let returnMatches = {};
-    for (var i = 0; i < matches.length; i += 2) {
-      const key = matches[i];
-      const value = matches[i+1];
+    let params = {};
+    for (var i = 0; i < groups.length; i += 2) {
+      const key = groups[i];
+      const value = groups[i+1];
 
-      returnMatches[key] = value;
+      params[key] = value;
     }
 
-    return returnMatches;
+    return params;
   }
 
 }
