1bc6be01b065c77b3b3adfbaf83bbc7ed3eff020
Nick White <nick@nickwhite.co>
2019-04-06 12:24:54 -0700

    Add task to fix bad album art

---
+++ beatnik/management/commands/fixartwork.py
@@ -0,0 +1,16 @@
+from beatnik.api_manager import ApiManager
+from beatnik.models import Music
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Converts all entries with non-Spotify sourced artwork to use Spotify artwork'
+
+    def handle(self, *args, **options):
+        sql = "SELECT * FROM beatnik_music WHERE artwork NOT ILIKE '%%scdn%%'"
+        bad_artwork = Music.objects.raw(sql)
+        api_manager = ApiManager()
+
+        for music in bad_artwork:
+            spotify_artwork = api_manager.link_parser.get_spotify_artwork(music)
+            music.artwork = spotify_artwork
+            music.save()
