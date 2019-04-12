d744c09bb74463f717cce20821854425218dddaa
Nick White <nick@nickwhite.co>
2019-04-11 22:00:14 -0700

    Clean up after myself

--- beatnik/urls.py
+++ beatnik/urls.py
@@ -1,13 +1,8 @@
-from django.urls import path, re_path
-from django.views.defaults import page_not_found
+from django.urls import re_path
 
 from beatnik.views import About, Index, Music, Rating, Search, Settings
 
-def custom_page_not_found(request):
-    return page_not_found(request, None)
-
 urlpatterns = [
-    re_path(r'^404/$', custom_page_not_found),
     re_path(r'^music/(?P<key>[0-9]+)/?', Music.as_view()),
     re_path(r'^search/?', Search.as_view()),
     re_path(r'^settings/?', Settings.as_view()),
