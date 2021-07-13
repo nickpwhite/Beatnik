8df2d244a32ec0c876d697d104a82e3ed3c53c10
Nick White <nick@nickwhite.co>
2021-07-12 20:02:12 -0700

    Redefine DJANGO_DEBUG

--- beatnik/settings.py
+++ beatnik/settings.py
@@ -27,7 +27,7 @@ os.environ['PROJECT_ROOT'] = PROJECT_ROOT;
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ['DJANGO_DEBUG'])
+DEBUG = os.environ['DJANGO_DEBUG'] == "True"
 
 ALLOWED_HOSTS = [
     'localhost',
