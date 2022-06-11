913513d92a10ffef7bca2de8ac582b68408c7d45
Nick White <nick@nickwhite.co>
2022-06-11 08:11:12 -0700

    Add railway host to development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "3ecb-157-131-154-125.ngrok.io"
+  config.hosts << "beatnik-development.up.railway.app"
 end
