69a82bb9eb3a9657a128baaee4c3dbd6f56d96d6
Nick White <nick@nickwhite.co>
2022-06-27 23:16:28 -0700

    Add domain to development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "beatnik-development.up.railway.app"
+  config.hosts << "www.beatnikapp.com"
 end
