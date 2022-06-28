d3c84af10415488976061a1db14a29fb8431d717
Nick White <nick@nickwhite.co>
2022-06-27 23:19:04 -0700

    Add dev domain to prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,4 +120,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.hosts << "www.beatnikapp.com"
+  config.hosts << "beatnik-development.up.railway.app"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_06_05_173450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "api_request", id: :serial, force: :cascade do |t|
     t.text "user_agent"
